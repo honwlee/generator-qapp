d450f3b44b5e049608b97ae31dd7f3f5c175cb95
honwlee <lihongwang0618@gmail.com>
2015-09-12 10:42:01 +0800

    add bundle.json

---
+++ app/templates/src/Activator.js
@@ -0,0 +1,34 @@
+define([
+    "qscript/lang/Class",
+    "utilhub/osgi/IBundleActivator",
+    "utilhub/osgi/BundleContext"
+], function(Class, IBundleActivator, BundleContext) {
+
+    var Activator = Class.declare({
+        "-parent-": Class,
+        "-interfaces-": [IBundleActivator],
+        "-protected-": {
+            "-fields-": {
+                "_": {}
+            },
+            "-methods-": {}
+        },
+        "-public-": {
+            "-attributes-": {},
+            "-methods-": {
+                "start": function( /*BundleContext*/ context) {
+                    //TODO
+                },
+
+                "stop": function( /*BundleContext*/ context) {
+                    //TODO
+                }
+
+            }
+        },
+        "-constructor-": {
+            "initialize": function() {}
+        }
+    });
+    return Activator;
+});
